431ab75975e3a22715b7f46d59c3bb3088011108
Christoph <christoph.hegemann@caffeine.ai>
2026-06-29 11:04:48 +0200

    regenerate bindings files (#19)

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_motoko "github.com/tree-sitter/tree-sitter-motoko/bindings/go"
+	tree_sitter_motoko "github.com/caffeinelabs/tree-sitter-motoko/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
